8f433a52fa64ac423c10ad064dd52f21c783bc2a
AveEtoKuzma <113539605+AveEtoKuzma@users.noreply.github.com>
2022-09-14 21:05:33 +0300

    lab1
    lab1

---
+++ ru/rsatu/java1/java1.3/HelloWorld.java
@@ -0,0 +1,11 @@
+package paket;
+
+public class HelloWorld{
+
+  public static void nach(){
+    System.out.println("Kupil");
+  }
+  public void machine(){
+    System.out.println("machiny");
+  }
+}
---
+++ ru/rsatu/java1/java1.3/NEHello.java
@@ -0,0 +1,9 @@
+package paket;
+import paket.HelloWorld;
+public class NEHello{
+  public static void main(String args[]){
+    HelloWorld obj = new HelloWorld();
+    HelloWorld.nach();
+    obj.machine();
+  }
+}
